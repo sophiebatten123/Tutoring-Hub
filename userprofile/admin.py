0de8ef024c3d2c516cf85c029a81e1aa15364738
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-02-23 16:28:40 +0000

    Add the user fields to the database and build upon the admin site making it easier to manage

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -6,4 +6,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Profile)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('full_name', 'slug', 'year_group', 'account_type')
+    search_fields = ['full_name', 'year_group', 'account_type']
+    prepopulated_fields = {'slug': ('full_name',)}
+    list_filter = ('account_type',)
     summernote_fields = ('about_me')
+    actions = ['approve_profile']
+
+    def approve_profile(self, request, queryset):
+        queryset.update(approve=True)
