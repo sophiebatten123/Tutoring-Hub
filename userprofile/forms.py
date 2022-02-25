c83d091a4455bd2a23ec0b89bdff05704203fa4d
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-02-25 15:27:56 +0000

    Initial stages of profile activation have been started on the views.py file

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -3,6 +3,14 @@ from django import forms
 
 
 class MakeProfile(forms.ModelForm):
+
     class Meta:
         model = Profile
-        fields =('full_name', 'year_group', 'about_me', 'featured_image')
+        fields = ('full_name', 'year_group', 'about_me', 'featured_image',)
+    
+    widgets = {
+        'full_name': forms.TextInput(attrs={'class': 'form-control'}),
+        'year_group': forms.TextInput(attrs={'class': 'form-control'}),
+        'about_me': forms.TextInput(attrs={'class': 'form-control'}),
+        'featured_image': forms.TextInput(attrs={'class': 'form-control'}),
+    }
