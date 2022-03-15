b6ce98fa97c1d756161f71dd037ea834bdca1b6a
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-03-15 18:03:52 +0000

    Table or the bookings section has been added to tutor one's page

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("exit-hide").style.display = "none";
     document.getElementById('create-btn').addEventListener('click', openTest);
     document.getElementById('exit-btn').addEventListener('click', closeTest);
+    document.getElementById('bookings').style.display="none";
+    document.getElementById('booking-btn').addEventListener('click', openBookings);
+    document.getElementById('upcoming-lessons').style.display="none";
+    document.getElementById('previous-lessons').style.display="none";
 
     function openTest() {
         document.getElementById("make-profile").style.display = "block";
@@ -22,4 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("create-btn").style.display = "inline";
         document.getElementById("create-btn-div").style.display = "flex";
     }
+
+    function openBookings() {
+        document.getElementById('bookings').style.display = "block";
+        document.getElementById('bookings-button').style.display="none";
+    }
+
 })
