624273ee2d8662e1b8634a636ba522f0e1c0f7a0
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-04-26 13:50:24 +0000

    User profiles have been made responsive to different screen sizes.

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => { 
 
-    document.getElementById("profile-btn").addEventListener("click", profilePage);
     document.getElementById("edit-btn").addEventListener("click", editProfile);
     document.getElementById("make-profile").style.display="none";
     document.getElementById("exit-btn").addEventListener("click", closeProfile);
     document.getElementById("");
 
-
-    function profilePage() {
-        document.getElementById("about-me").style.display="block";
-        document.getElementById("make-profile").style.display="none";
-    }
 
     function editProfile() {
         document.getElementById("make-profile").style.display="block";
